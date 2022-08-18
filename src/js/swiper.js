b9d6bc1a753260e2716b53af84e9fc2bd47e32e6
Igor Bodrov <bodrovigor02@gmail.com>
2022-08-18 17:58:42 +0300

    feat: 🎸 Добавил разметку, стилизацию

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -3,12 +3,19 @@ import Swiper from 'swiper/bundle';
 //Слайдер из блока about
 const heading_slider = new Swiper('.heading__slider', {
 
-  direction: 'vertical',
   loop: true,
 
-  // navigation: {
-  //   nextEl: '.about__slider-button--next',
-  //   prevEl: '.about__slider-button--prev',
-  // },
+  breakpoints: {
+    320: {
+      direction: 'horizontal',
+    },
+    
+    768: {
+      direction: 'vertical',
+    }
+  },
 
+  pagination: {
+    el: '.swiper-pagination',
+  },
 }); 
