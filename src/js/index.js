fe34cb84ea5ef284ad688ab7febd216a80af8c59
Igor Bodrov <bodrovigor02@gmail.com>
2022-08-19 17:55:24 +0300

    feat: 🎸 Добавил разметку, стилизацию, адаптивность

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,7 @@
 import { disableBodyScroll, enableBodyScroll } from 'body-scroll-lock';
 
+
+//Мобильное меню
 let burger = document.getElementById('nav-burger');
 let mobileMenu = document.getElementById('nav-mobile-menu');
 
@@ -14,3 +16,51 @@ burger.addEventListener('click', () => {
     enableBodyScroll(mobileMenu);
   }
 })
+
+//Табы в блоке engine
+
+let engineTabsButtons = document.querySelectorAll('.engin__tabs-button');
+let engineTabsItems = document.querySelectorAll('.engin__tabs-itemset');
+
+engineTabsButtons.forEach((item) => {
+  item.addEventListener('click', (e) => {
+    
+    e.preventDefault();
+    const id = e.target.getAttribute('href').replace('#', '');
+
+    engineTabsButtons.forEach(
+      (child) => child.classList.remove('engin__tabs-button--active')
+    );
+    engineTabsItems.forEach(
+      (child) => child.classList.remove('engin__tabs-itemset--active')
+    );
+
+    item.classList.add('engin__tabs-button--active');
+    document.getElementById(id).classList.add('engin__tabs-itemset--active');
+  })
+})
+
+//Табы в блоке consult
+
+let consultTabsButtons = document.querySelectorAll('.consult__tabs-button');
+let consultTabsItems = document.querySelectorAll('.consult__tabs-itemset');
+
+consultTabsButtons.forEach((item) => {
+  item.addEventListener('click', (e) => {
+
+    e.preventDefault();
+    const id = e.target.getAttribute('href').replace('#', '');
+
+    consultTabsButtons.forEach(
+      (child) => child.classList.remove('consult__tabs-button--active')
+    );
+    consultTabsItems.forEach(
+      (child) => child.classList.remove('consult__tabs-itemset--active')
+    );
+
+    item.classList.add('consult__tabs-button--active');
+    document.getElementById(id).classList.add('consult__tabs-itemset--active');
+  })
+})
+
+
