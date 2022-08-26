9281e4bfabb3081c6ee264591d68a1201d7344b5
Igor Bodrov <bodrovigor02@gmail.com>
2022-08-26 15:10:12 +0300

    feat: 🎸 Добавил модальные окна для всех форм на странице

---
+++ src/js/success-modal.js
@@ -0,0 +1,35 @@
+import { disableBodyScroll, enableBodyScroll } from 'body-scroll-lock';
+
+//Открытие модального окна с сообщением об успехе
+
+let successModal = document.getElementById('success-modal');
+let successModalForm = document.querySelectorAll('#success-modal-form');
+let successModalClose = document.getElementById('success-modal-close');
+
+successModalForm.forEach((element) => {
+  element.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    successModal.classList.add('success-modal--opened');
+    disableBodyScroll(element);
+
+    Array.from(element.elements).filter(tag => ["input", "textarea"]).forEach((item) => {
+      item.value = '';
+    })
+  })
+})
+
+successModalClose.addEventListener('click', () => {
+  successModal.classList.remove('success-modal--opened');
+  enableBodyScroll(successModal);
+})
+
+successModal.addEventListener('click', (e) => {
+  if (e.target !== e.currentTarget) {
+    return;
+  }
+  else {
+    successModal.classList.remove('success-modal--opened');
+    enableBodyScroll(successModal);
+  }
+})
